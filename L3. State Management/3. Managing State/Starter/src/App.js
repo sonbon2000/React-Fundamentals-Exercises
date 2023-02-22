2f7f80bcd1ee0f9e37fdc28f0ef5be84a7333831
sonbon2000 <75024108+sonbon2000@users.noreply.github.com>
2023-02-22 13:25:30 +0000

    L3-ManagingState

--- L3. State Management/3. Managing State/Starter/src/App.js
+++ L3. State Management/3. Managing State/Starter/src/App.js
@@ -1,12 +1,19 @@
 import corgi from "./corgi.jpg";
 import "./App.css";
+import { useState } from "react";
+
 
 const App = () => {
+  const [likes, setLikes] = useState(0);
+  const increaseLikes = () => {
+    setLikes(likes + 1);
+  };
   return (
     <div className="container">
       <h2>Like this photo!</h2>
       <img src={corgi} alt="Corgi"></img>
       <p>Amount of likes: </p>
+      <button onClick={increaseLikes}>Increase</button>
     </div>
   );
 };
